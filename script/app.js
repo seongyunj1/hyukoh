d821f33caab140bef804d4ae47168a09c5397308
seongyunj1 <149452926+seongyunj1@users.noreply.github.com>
2024-02-28 01:38:21 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -1,36 +1,33 @@
 let tl = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '.logo',
         start: '0%',
         end: '80%',
-        scrub: 1 
+        scrub: 1
     },
 });
 let tl2 = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '.logo',
         start: '0%',
         end: '60%',
-        scrub: 1 
+        scrub: 1
     },
 });
 
 let tl4 = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '.logo',
         start: '0%',
         end: '200%',
-        scrub: 1 ,
+        scrub: 1,
     },
 });
 
-
-
-
 // tl.fromTo('.sliding-text',{y:0},{y:-400});
 
-tl2.fromTo('.logo',{scale:8},
-{scale:1, top:"1rem", left:"1rem", x:"140%", y:"60%"});
+tl2.fromTo('.logo', { scale: 8 },
+    { scale: 1, top: "1rem", left: "1rem", x: "120%", y: "60%" });
 
 
 // let tl3 = gsap.timeline({
@@ -46,6 +43,17 @@ tl2.fromTo('.logo',{scale:8},
 
 //로고 효과
 
+
+
+$(function () {
+    $(".allslide").slick();
+    $(".allbox").slick({
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 3
+    });
+
+});
 
 
 
