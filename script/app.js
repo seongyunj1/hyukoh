8ffb15833088cc7b25c036a97e8e7768088e34c5
seongyunj1 <149452926+seongyunj1@users.noreply.github.com>
2024-03-06 15:52:03 +0900

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -1,79 +1,121 @@
 
 
 let tl = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '#menu',
         start: '0%',
         end: '60%',
-        scrub: 1 
+        scrub: 1
     },
 });
 let tl2 = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '#menu',
         start: '0%',
         end: '80%',
-        scrub: 1 
+        scrub: 1
     },
 });
 
 let tl4 = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '#menu',
         start: '0%',
         end: '100%',
-        scrub: 1 ,
+        scrub: 1,
     },
 });
 
 
-tl2.fromTo('.logo',{scale:6},
-{scale:1, top:"4%", left:"5%", x:"50%", y:"50%"});
+tl2.fromTo('.logo', { scale: 6 },
+    { scale: 1, top: "45%", left: "5%", x: "50%", y: "50%" });
 
 
 
 let tl3 = gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '#menu',
         start: '0%',
         end: '10000%',
-        scrub: 1 ,
+        scrub: 1,
         pin: true,
-        pinSpacing:false,
+        pinSpacing: false,
     },
 });
 
+/////////////////////
 
+
+$(function(){
+$(".allslide").slick({
+    centerMode: true,
+    centerPadding: '5px',
+    slidesToShow: 1,
+})
+});
+// $(".Mslide").slick({
+//     centerMode: true,
+//     centerPadding: '10px',
+//     slidesToShow: 1,
+// })
+// });
 
 
 ////////////////////////////////
 
 
 $(function () {
-    $(".allslide").slick();
     $(".allbox").slick({
         centerMode: true,
         centerPadding: '10px',
-        slidesToShow: 3
+        slidesToShow: 3,
+        responsive: [
+            {
+              breakpoint: 770,
+              settings: {
+                slidesToShow: 1,
+                infinite: true,
+              }
+            }
+        
+          ]
     });
 
 });
 ////////////////////////////////
 
+// $(function(){
+//     $(".slick-slide .cover img").on({
+//         mouseover:function(){
+//             $(".slick-center .LP img").css({
+//                 'transform':'rotate(90deg)'
+                
+//             })
+//         }
+//     })
+// });
 
-$(function(){
-  
-    $(".menuB").on({
-        click:function(){
-            $("header").stop(true).slideDown('fast');
+
+
+
+//////////////////
+
+
+$(function () {
+
+    $("#menu .menuB").on({
+        click: function () {
+            $("header").on().css({display:'block'});
             $(".closeB").stop(true).show();
             $(".menuB").stop(true).hide();
         }
     });
 
+
+
     $(".closeB").on({
-        click:function(){
-            $("header").stop(true).slideUp('fast');
+        click: function () {
+            $("header").on().css({display:'none'});
             $(".closeB").stop(true).hide();
             $(".menuB").stop(true).show();
         }
